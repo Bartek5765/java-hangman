c4da33537889d1d117a683cd88cc52e5000d0c2c
sabala <sabalaja@gmail.com>
2025-02-09 13:58:33 +0100

    printer

--- src/main/java/pl/edu/agh/hangman/Printer.java
+++ src/main/java/pl/edu/agh/hangman/Printer.java
@@ -1,4 +1,20 @@
 package pl.edu.agh.hangman;
 
+import java.util.Set;
+
+import static pl.edu.agh.hangman.Hangman.HANGMANPICS;
+
 public class Printer {
+    private static void printHangMan(int n) {
+        System.out.println(HANGMANPICS[n]);
+    }
+
+    private static void printWorld(char[] arrOfGuessedLetters) {
+        System.out.println(arrOfGuessedLetters);
+    }
+
+    public static void printOutput(int n, char[] arrOfGuessedLetters) {
+        printHangMan(n);
+        printWorld(arrOfGuessedLetters);
+    }
 }
