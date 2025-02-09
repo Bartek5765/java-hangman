61ac23abf1213a1cbf8a0e220f53d64afce71093
sabala <sabalaja@gmail.com>
2025-02-09 14:48:47 +0100

    reader updated

--- src/main/java/pl/edu/agh/hangman/Reader.java
+++ src/main/java/pl/edu/agh/hangman/Reader.java
@@ -4,32 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Reader {
-    //konstruktor wczytuje zawartość pliku z listy
-//    public void getTextFile() {
-////        try {
-//
-//            printInputStream(is);
-////            words = is.stream().map(this::deleteSpecialCharacter).filter(line -> !line.isEmpty()).collect(Collectors.toList());
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////            words = List.of();
-////        }
-//    }
-
-    private static void printInputStream() {
-
-
-    }
     public static String getRandomWord() {
         InputStream is = Reader.class.getClassLoader().getResourceAsStream("slowa.txt");
         Random random = new Random();
@@ -48,7 +28,6 @@ public class Reader {
             e.printStackTrace();
         }
 
-        printInputStream();
         if (words.isEmpty()) {
             return "";
         }
